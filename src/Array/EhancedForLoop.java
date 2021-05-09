64d5fe14b557ea980d1e40bee21591e46868d673
LG <LG@DESKTOP-AS7N5K1>
2021-05-09 17:39:24 +0900

    Initial

---
+++ src/Array/EhancedForLoop.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class EhancedForLoop {
+	public static void main(String[] args) {
+		String[] arr = {"JAVA", "PYTHON", "JAVASCRIPT", "C++", "C#"};
+		
+		for(String lang : arr) {
+			System.out.println(lang);
+		}
+	}
+}
---
+++ src/Array/ObejctArray1.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class ObejctArray1 {
+	private String name;
+	private int number;
+	
+	public ObejctArray1() {
+	}
+	public ObejctArray1(String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
